7d25ba4912f6472024b8982236c59edf3a467e8f
JYOTHIKANAVYASRICHEGONDI <85726266+jyothikachegondi@users.noreply.github.com>
2023-07-19 10:28:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function redrose(){
+    document.getElementById("flower").src="https://www.citypng.com/public/uploads/preview/download-red-rose-flower-illustration-png-11638815982urijylrjie.png";
+}
+function whiterose(){
+    document.getElementById("flower").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdbCaEojjxQsielcKUFbj326ksjklCbof0g&usqp=CAU";
+}
+function pinkrose()
+{
+    document.getElementById("flower").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp7qR1JPLBr49PEpBPkiGw02lyBXgl646lMQ&usqp=CAU";
+}
